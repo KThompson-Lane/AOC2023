30bcb9f4ef6781eee99db454ddde603f22c35af3
KThompson-Lane <kialltlane1908@gmail.com>
2023-12-01 11:57:47 +0000

    Add input helper method

---
+++ HelperMethods/InputHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace AOC2023.HelperMethods;
+
+public static class InputHelper
+{
+    public static IEnumerable<string> GetInput(string task) => 
+        File.ReadLines($@"C:\Users\kiallthompson-lane\Documents\personal-projects\AOC2023\{task}\input.txt");
+}
