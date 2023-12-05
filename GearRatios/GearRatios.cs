8d4b5454acffea73891ed85c87e9ea9f2c6b0dc9
KThompson-Lane <kialltlane1908@gmail.com>
2023-12-05 12:45:30 +0000

    day 3

---
+++ GearRatios/GearRatios.cs
@@ -0,0 +1,49 @@
+﻿using AOC2023.HelperMethods;
+
+namespace AOC2023.GearRatios;
+
+public class GearRatios
+{
+    public static void GetSumOfPartNumbers()
+    {
+        var input = InputHelper.GetInput(nameof(GearRatios));
+        var schematic = new Schematic();
+        List<int> partNumbers = new List<int>();
+        foreach (var line in input)
+        {
+            schematic.AddRow(line);
+        }
+        var symbols = schematic.grid.SelectMany((list, row) => list.Select((c, col) => (row, col, c))).Where(x => x.c != '.' && !char.IsDigit(x.c));
+
+        foreach (var (row, col, symbol) in symbols)
+        {
+            var adjacentChars = schematic.GetAdjacentChars((row, col)).Where(x => char.IsDigit(x.Item1));
+            partNumbers.AddRange(adjacentChars.Select(x => schematic.GetPartNumber(x.Item2)).Distinct());
+        }
+
+        Console.Out.WriteLine(partNumbers.Sum());
+    }
+    public static void GetSumOfGearRatio()
+    {
+        var input = InputHelper.GetInput(nameof(GearRatios));
+        var schematic = new Schematic();
+        int sum = 0;
+        foreach (var line in input)
+        {
+            schematic.AddRow(line);
+        }
+        var symbols = schematic.grid.SelectMany((list, row) => list.Select((c, col) => (row, col, c))).Where(x => x.c != '.' && !char.IsDigit(x.c));
+
+        foreach (var (row, col, symbol) in symbols)
+        {
+            var adjacentChars = schematic.GetAdjacentChars((row, col)).Where(x => char.IsDigit(x.Item1));
+            var gears = adjacentChars.Select(x => schematic.GetPartNumber(x.Item2)).Distinct().ToList();
+            if (gears.Count == 2)
+            {
+                sum += gears[0] * gears[1];
+            }
+        }
+
+        Console.Out.WriteLine(sum);
+    }
+}
---
+++ GearRatios/Schematic.cs
@@ -0,0 +1,49 @@
+﻿namespace AOC2023.GearRatios;
+
+public class Schematic
+{
+    //  List of chars in the form row, column
+    public List<List<char>> grid = new List<List<char>>();
+
+    public void AddRow(IEnumerable<char> row)
+    {
+        grid.Add(row.ToList());
+    }
+
+    public IEnumerable<(char, (int row, int col))> GetAdjacentChars((int row, int col) index)
+    {
+        yield return GetElement((index.row+1, index.col+1));
+        yield return GetElement((index.row+1, index.col));
+        yield return GetElement((index.row+1, index.col-1));
+        yield return GetElement((index.row, index.col+1));
+        yield return GetElement((index.row, index.col-1));
+        yield return GetElement((index.row-1, index.col+1));
+        yield return GetElement((index.row-1, index.col));
+        yield return GetElement((index.row-1, index.col-1));
+    }
+
+    private (char, (int row, int col)) GetElement((int row, int col) index)
+    {
+        return ((grid.ElementAtOrDefault(index.row)?.ElementAtOrDefault(index.col) ?? '.'), (index.row, index.col));
+    }
+    
+    public int GetPartNumber((int row, int col) index)
+    {
+        var start = index.col;
+        char item;
+        do
+        {
+            item = grid.ElementAtOrDefault(index.row)?.ElementAtOrDefault(--start) ?? '.';
+        } while (char.IsDigit(item));
+
+        string partNumber = "";
+        item = grid.ElementAtOrDefault(index.row)?.ElementAtOrDefault(++start) ?? '.';
+        while (char.IsDigit(item))
+        {
+            partNumber += item;
+            item = grid.ElementAtOrDefault(index.row)?.ElementAtOrDefault(++start) ?? '.';
+        }
+
+        return int.Parse(partNumber);
+    }
+}
