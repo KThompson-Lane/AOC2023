d96f577bdff4230d584d35fcd85dc26ee80bcb9c
KThompson-Lane <kialltlane1908@gmail.com>
2023-12-05 12:45:24 +0000

    day 2

---
+++ CubeConundrum/CubeConundrum.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using AOC2023.HelperMethods;
+
+namespace AOC2023.CubeConundrum;
+
+public class CubeConundrum
+{
+    public static void GetValidGames()
+    {
+        var confguration = new List<(Game.Colour, int)>()
+        {
+            new(Game.Colour.Red, 12),
+            new(Game.Colour.Green, 13),
+            new(Game.Colour.Blue, 14),
+        };
+        var sum = GetGames().Sum(x => x.Valid(confguration) ? x.Id : 0);
+        Console.Out.WriteLine(sum);
+    }
+
+    public static void GetSumOfPowers()
+    {
+        var sum = GetGames().Sum(x => x.GetMinimumCubes().Aggregate(1, (power, next) => power * next.Item2));
+
+        Console.Out.WriteLine(sum);
+    }
+
+    private static IEnumerable<Game> GetGames()
+    {
+        var input = InputHelper.GetInput(nameof(CubeConundrum));
+        foreach (var line in input)
+        {
+            var parts = line.Split(':');
+            var gameId = int.Parse(string.Concat(parts[0].Where(char.IsDigit)));
+            var sets = parts[1].Split(';');
+            var game = new Game(gameId);
+            foreach (var set in sets)
+            {
+                var matches = Regex.Matches(set, @"(\d+)\s(\w+)");
+                var cubes = matches.Select(match => (Game.GetColour(match.Groups[2].Value), int.Parse(match.Groups[1].Value)));
+                game.AddSet(cubes);
+            }
+            yield return game;
+        }
+    }
+}
---
+++ CubeConundrum/Game.cs
@@ -0,0 +1,61 @@
+﻿namespace AOC2023.CubeConundrum;
+
+public class Game
+{
+    public int Id { get;}
+
+    public Game(int id)
+    {
+        Id = id;
+    }
+    
+    public enum Colour
+    {
+        Red,
+        Blue,
+        Green
+    }
+
+    public static Colour GetColour(string col) =>
+        col.ToLower() switch
+        {
+            "red" => Colour.Red,
+            "blue" => Colour.Blue,
+            "green" => Colour.Green,
+            _ => throw new ArgumentOutOfRangeException(nameof(col), "Invalid colour")
+        };
+
+    private readonly List<HashSet<(Colour, int)>> _cubes = new();
+
+    public void AddSet(IEnumerable<(Colour, int)> set)
+    {
+        var newSet = new HashSet<(Colour, int)>();
+        foreach (var group in set)
+        {
+            newSet.Add(group);
+        }
+
+        _cubes.Add(newSet);
+    }
+
+    public bool Valid(IEnumerable<(Colour, int)> configuration)
+    {
+        foreach (var (colour, amount) in configuration)
+        {
+            var invalid = _cubes.Any(set => set.Where(grp => grp.Item1 == colour).Any(grp => grp.Item2 > amount));
+
+            if (invalid)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public IEnumerable<(Colour, int)> GetMinimumCubes()
+    {
+        var numbers = _cubes.SelectMany(x => x.Select(y => y)).GroupBy(x => x.Item1);
+        var biggest = numbers.Select(x => x.MaxBy(y => y.Item2));
+        return biggest;
+    }
+}
