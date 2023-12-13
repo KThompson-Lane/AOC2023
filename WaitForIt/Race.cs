8ea069af1979658f7df2fdc6184793986e8acb13
KThompson-Lane <kialltlane1908@gmail.com>
2023-12-13 15:33:13 +0000

    day 6

---
+++ WaitForIt/Race.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+
+namespace AOC2023.WaitForIt;
+
+public class Race
+{
+    public long Time;
+    public long Distance;
+
+    public Race((long time, long distance) race)
+    {
+        Time = race.time;
+        Distance = race.distance;
+    }
+
+    public int GetPossibleWins()
+    {
+        int distanceCovered = 0;
+        int possibleWins = 0;
+        for (int i = 1; i < Time; i++)
+        {
+            distanceCovered = 0;
+            var remainingTime = Time - i;
+            for (int j = 0; j < remainingTime; j++)
+            {
+                distanceCovered += i;
+            }
+            if (distanceCovered > Distance)
+                possibleWins++;
+        }
+
+        return possibleWins;
+    }
+
+    public int GetPossibleWinsImproved()
+    {
+        // Furthest you can go is (T/2 * T-T/2)
+        // D = T-x * y == D = T-y * x
+        var possibleWins = 0;
+        var halfTime = (Time / 2);
+        for (long i = halfTime; i > 1; i--)
+        {
+            var distance = i * (Time - i);
+            if (distance <= Distance)
+                break;
+            possibleWins += 2;
+        }
+
+        if (Time % 2 == 0)
+            possibleWins--;
+        return possibleWins;
+    }
+
+    public int GetPossibleWinsFinal()
+    {
+        // Similar to above but counting up
+        var halfTime = (Time / 2);
+        int testTime = 1;
+        do
+        {
+            var distanceCovered = testTime * (Time - testTime);
+            if (distanceCovered <= Distance)
+                testTime++;
+            else
+                break;
+        } while (true);
+
+        var possibleWins = (int)(halfTime - testTime) * 2;
+        if (Time % 2 != 0)
+            possibleWins++;
+        return ++possibleWins;
+    }
+    
+    public static void Tests()
+    {
+        var times = new List<long>();
+        // Get Input race
+        var inputRace = new Race((1088698992, 9003797889445));
+        
+        //  Test First Version
+        var sw = Stopwatch.StartNew();
+        var test = inputRace.GetPossibleWinsImproved();
+        Console.Out.WriteLine(test);
+        sw.Stop();
+        times.Add(sw.ElapsedMilliseconds);
+        
+        // Test improved version
+        sw.Restart();
+        test = inputRace.GetPossibleWinsImproved();
+        Console.Out.WriteLine(test);
+        sw.Stop();
+        times.Add(sw.ElapsedMilliseconds);
+        
+        // Test final version
+        sw.Restart();
+        test = inputRace.GetPossibleWinsFinal();
+        Console.Out.WriteLine(test);
+        sw.Stop();
+        times.Add(sw.ElapsedMilliseconds);
+        foreach (var time in times)
+        {
+            Console.Out.WriteLine(time);
+        }
+    }
+}
---
+++ WaitForIt/WaitForIt.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+using AOC2023.HelperMethods;
+
+namespace AOC2023.WaitForIt;
+
+public class WaitForIt
+{
+    
+    public static void PartOne()
+    {
+        var input = InputHelper.GetInput(nameof(WaitForIt));
+        var races = GetRaces(input);
+        
+        var possibleScenarios = races.Aggregate(1, (scenarios, next) => scenarios * next.GetPossibleWinsFinal());
+        Console.Out.WriteLine(possibleScenarios);
+    }
+
+    public static void PartTwo()
+    {
+        var input = InputHelper.GetInput(nameof(WaitForIt));
+        var race = GetRace(input);
+        Console.Out.WriteLine(race.GetPossibleWinsFinal());
+    }
+
+    private static Race GetRace(IEnumerable<string> input)
+    {
+        using var enumerator = input.GetEnumerator();
+        if (!enumerator.MoveNext())
+            throw new IOException("Input has no time!");
+        var time = long.Parse(Regex.Matches(enumerator.Current, @"\d+").SelectMany(x => x.Value).ToArray());
+        if(!enumerator.MoveNext())
+            throw new IOException("Input has no distance!");
+        
+        var distance = long.Parse(Regex.Matches(enumerator.Current, @"\d+").SelectMany(x => x.Value).ToArray());
+        return new Race((time, distance));
+    }
+
+    private static IEnumerable<Race> GetRaces(IEnumerable<string> input)
+    {
+        using var enumerator = input.GetEnumerator();
+        if (!enumerator.MoveNext())
+            throw new IOException("Input has no times!");
+        var times = Regex.Matches(enumerator.Current, @"\d+").Select(x => uint.Parse(x.Value));
+        if(!enumerator.MoveNext())
+            throw new IOException("Input has no distances!");
+        var distances = Regex.Matches(enumerator.Current, @"\d+").Select(x => uint.Parse(x.Value));
+        return times.Zip(distances).Select(x => new Race(x));
+    }
+}
